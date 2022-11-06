f0415c3c68d2dbc8f6dbb8b4d075998a5b12f8cc
Carlos Edilson Junior <40894261+carlosedilsonjr@users.noreply.github.com>
2022-11-06 03:45:52 -0300

    show the list of user's polls

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,11 +1,42 @@
-import { Icon, VStack } from "native-base";
+import { FlatList, Icon, useToast, VStack } from "native-base";
 import { Octicons } from '@expo/vector-icons'
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { api } from "../services/api";
+import { useCallback, useState } from "react";
+import { PoolCard, PoolCardProps } from "../components/PoolCard";
+import { Loading } from "../components/Loading";
+import { EmptyPoolList } from "../components/EmptyPoolList";
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [pools, setPools] = useState<PoolCardProps[]>([])
   const { navigate } = useNavigation()
+  const toast = useToast()
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true)
+      const response = await api.get('/pools')
+      setPools(response.data.pools)
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: 'Não foi possível carregar os bolões.',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchPools()
+  }, []))
+
 
   return (
     <VStack flex={1} bgColor={'gray.900'}>
@@ -18,6 +49,18 @@ export function Pools() {
           onPress={() => navigate('find')}
         />
       </VStack>
+
+      {isLoading ? <Loading /> :
+        <FlatList
+          data={pools}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          px={5}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ pb: 10 }}
+          ListEmptyComponent={() => <EmptyPoolList />}
+        />}
+
     </VStack>
   )
 }
